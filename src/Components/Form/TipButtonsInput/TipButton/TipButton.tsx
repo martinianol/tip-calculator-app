0851ef35ff5af6c70a776005a7f051d4374782ec
Martiniano-Bluealba <martiniano.leguizamon@bluealba.com>
2025-06-15 13:11:14 -0300

    Add correct styling to tip-buttons

--- src/Components/Form/TipButtonsInput/TipButton/TipButton.tsx
+++ src/Components/Form/TipButtonsInput/TipButton/TipButton.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 interface TipButtonProps {
   val: number | string;
@@ -6,19 +7,19 @@ interface TipButtonProps {
   isActive?: boolean;
   isCustom?: boolean;
 }
-const standardClassName =
-  "text-center py-2 px-4 border-none rounded-[5px] text-preset3 text-white bg-green-900 hover:bg-green-200 hover:text-green-900";
-const activeClassName = "bg-green-200 text-green-900 border-none";
-const customClassName = "bg-grey-50 text-grey-550";
+const baseStyles =
+  "text-center py-2 px-4 border-none rounded-[5px] text-preset3 text-white bg-green-900 hover:bg-green-200 hover:text-green-900 cursor-pointer";
+const customStyles = "bg-grey-50 text-grey-550";
+const activeStyles = "bg-green-400 text-green-900";
 
 const TipButton = ({ val, onClick, isActive, isCustom }: TipButtonProps) => {
   const id = `tip-${val}`;
-  const buttonClassName = clsx(standardClassName, {
-    [activeClassName]: isActive,
-    [customClassName]: isCustom,
-  });
 
+  const buttonClassName = twMerge(
+    clsx(baseStyles, isCustom && customStyles, isActive && activeStyles)
+  );
   const displayVal = val + (typeof val === "number" ? "%" : "");
+
   return (
     <label htmlFor={id} className={buttonClassName}>
       <input
