75738f6a53071d86516aea501a823bb1b83f5c0d
Martiniano-Bluealba <martiniano.leguizamon@bluealba.com>
2025-06-25 17:21:28 -0300

    Display % in Custom input

--- src/Components/Form/TipButtonsInput/TipButton/CustomTipButton.tsx
+++ src/Components/Form/TipButtonsInput/TipButton/CustomTipButton.tsx
@@ -10,25 +10,36 @@ interface CustomTipButton {
 
 const CustomTipButton = ({ val, onChange, isActive }: CustomTipButton) => {
   const baseStyles =
-    "cursor-pointer bg-grey-50 text-center outline-none min-w-0 text-green-900 placeholder-grey-550 border-2 border-transparent rounded-[5px]";
+    "cursor-pointer bg-grey-50 text-center outline-none min-w-0 w-full h-full text-green-900 placeholder-grey-550 border-2 border-transparent rounded-[5px]";
   const focusStyles = "focus:bg-grey-50 focus:border-green-400";
   const inputStyles = `text-preset3 ${twMerge(
     clsx(baseStyles, isActive && activeStyles, focusStyles)
   )}`;
 
   const displayValue = val === null ? "" : val;
+  const rightForPercentage = val && val >= 10 ? "27px" : "35px";
+
   return (
-    <input
-      id="custom-tip-input"
-      className={inputStyles}
-      type="number"
-      value={displayValue}
-      onChange={(e) => onChange(Number(e.target.value))}
-      placeholder="Custom"
-      min={0}
-      max={100}
-      step={1}
-    />
+    <div className="relative">
+      <input
+        id="custom-tip-input"
+        className={inputStyles}
+        type="number"
+        value={displayValue}
+        onChange={(e) => onChange(Number(e.target.value))}
+        placeholder="Custom"
+        min={0}
+        max={100}
+        step={1}
+      />
+      {isActive && (
+        <span
+          className={`absolute inset-y-0 right-[${rightForPercentage}] flex items-center pointer-events-none text-green-900 text-preset3`}
+        >
+          %
+        </span>
+      )}
+    </div>
   );
 };
 
